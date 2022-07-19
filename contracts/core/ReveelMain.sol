2e385ae7054a9bc0a8724e9ec4497fb5982df254
Tirtha Sarker <tsark99@gmail.com>
2022-07-19 18:46:12 +0530

    Reveel Main contract: Multi revenue path addition + events

--- contracts/core/ReveelMain.sol
+++ contracts/core/ReveelMain.sol
@@ -1,40 +1,101 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.9;
 
 import "openzeppelin-solidity/contracts/proxy/Clones.sol";
 import "openzeppelin-solidity/contracts/access/Ownable.sol";
 import "./RevenuePath.sol";
 
 contract ReveelMain is Ownable {
-    uint256 platformFee;
-    address platformWallet;
-
+    
+    //@notice Fee percentage that will be applicable for additional tiers
+    uint256 public platformFee;
+    //@notice Addres of platform wallet to collect fees
+    address public platformWallet;
+    //@notice The list of revenue path contracts
     RevenuePath[] public revenuePaths;
-    event RevenuePathCreated(RevenuePath path);
-
+    //@notice The revenue path contract address who's bytecode will be used for cloning
     address public libraryAddress;
 
-    constructor(address _libraryAddress) {
+    /********************************
+     *           EVENTS              *
+     ********************************/
+    /** @notice Emits when a new revenue path is created
+     * @param path The address of the new revenue path
+     */
+    event RevenuePathCreated(RevenuePath path);
+    /** @notice Updates the libaray contract address
+     * @param newLibrary The address of the library contract
+     */
+    event UpdatedLibraryAddress(address newLibrary);
+
+    /** @notice Updates the platform fee percentage
+     * @param newFeePercentage The new fee percentage
+     */
+    event UpdatedPlatformFee(uint256 newFeePercentage);
+
+    /** @notice Updates the platform fee collecting wallet
+     * @param newWallet The new fee collecting wallet
+     */
+    event UpdatedPlatformWallet(address newWallet);
+
+    /** @notice Intialize the Revenue main contract
+     * @param _libraryAddress The revenue path contract address who's bytecode will be used for cloning
+     * @param _platformFee The platform fee percentage
+     * @param _platformWallet The platform fee collector wallet
+     */
+    constructor(address _libraryAddress, uint256 _platformFee,address _platformWallet) {
         libraryAddress = _libraryAddress;
+        platformFee = _platformFee;
+        platformWallet = _platformWallet;
     }
 
+    /** @notice Sets the libaray contract address
+     * @param _libraryAddress The address of the library contract
+     */
     function setLibraryAddress(address _libraryAddress) external onlyOwner {
         libraryAddress = _libraryAddress;
+        emit UpdatedLibraryAddress(libraryAddress);
     }
 
-    function setPlatformFee(uint256 newFee) external onlyOwner {
-        platformFee = newFee;
+
+    /** @notice Set the platform fee percentage
+     * @param newFeePercentage The new fee percentage
+     */
+    function setPlatformFee(uint256 newFeePercentage) external onlyOwner {
+        platformFee = newFeePercentage;
+        emit UpdatedPlatformFee(platformFee);
     }
 
-    function createRevenuePath(address[] memory _walletList, uint256[] memory _distribution) external {
+    /** @notice Set the platform fee collecting wallet
+     * @param newWallet The new fee collecting wallet
+     */
+    function setPlatformWallet(address newWallet) external onlyOwner {
+        platformWallet = newWallet;
+        emit UpdatedPlatformWallet(platformWallet);
+    }
+
+    /** @notice Create a new revenue path
+     * @param _walletList A nested array of member wallet list
+     * @param _distribution A nested array of distribution percentages
+     * @param tierLimit A sequential list of tier limit
+     * @param isImmutable Set this flag to true if immutable
+     */
+    function createRevenuePath(
+        address[][] memory _walletList,
+        uint256[][] memory _distribution,
+        uint256[] memory tierLimit,
+        bool isImmutable
+    ) external {
         RevenuePath path = RevenuePath(payable(Clones.clone(libraryAddress)));
-        path.initialize(_walletList, _distribution, platformFee, platformWallet);
-
         revenuePaths.push(path);
+
+        path.initialize(_walletList, _distribution, tierLimit, platformFee, platformWallet, isImmutable, msg.sender);
         emit RevenuePathCreated(path);
     }
 
-    function getPaths() external view returns (RevenuePath[] memory) {
-        return revenuePaths;
+    /** @notice Get the list of revenue paths deployed and count
+     */
+    function getPaths() external view returns (RevenuePath[] memory, uint256 totalPaths) {
+        return (revenuePaths, revenuePaths.length);
     }
 }
