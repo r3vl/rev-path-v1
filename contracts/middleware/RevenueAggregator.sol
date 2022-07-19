7869399dd284bb89f7c8643817a89616a588f09b
Tirtha Sarker <tsark99@gmail.com>
2022-07-19 18:47:22 +0530

    Aggregator Natspec Added

--- contracts/middleware/RevenueAggregator.sol
+++ contracts/middleware/RevenueAggregator.sol
@@ -1,12 +1,19 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.9;
 
 contract RevenueAggregator {
+
+    /** @notice Emits when a withdrawal takes place
+     * @param path The address of the revenue path
+     * @param status Whether the withdrawal suceeded
+     */
     event WithdrawStatus(address indexed path, bool indexed status, bytes result);
 
-    // constructor() {}
-
+    /** @notice Batch withdrawal request for ETH across revenue paths
+     * @param paths List of revenue paths
+     * @param targetWallet The wallet for which the withdrawal is being made
+     */
     function withdrawPathEth(address[] calldata paths, address targetWallet) external {
         uint256 pathLength = paths.length;
 
@@ -19,6 +26,11 @@ contract RevenueAggregator {
         }
     }
 
+    /** @notice Batch withdrawal request for ERC20 across revenue paths
+     * @param paths List of revenue paths
+     * @param targetWallet The wallet for which the withdrawal is being made
+     * @param tokenAddress The ERC20 token for which the request is being made.
+     */
     function withdrawPathErc20(address[] calldata paths, address targetWallet, address tokenAddress) external {
         uint256 pathLength = paths.length;
 
