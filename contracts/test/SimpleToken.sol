80b6d1e26f30230853894e10b7eb314acbe73f89
appleseed-iii <appleseed-iii@protonmail.com>
2022-08-22 16:46:33 -0500

    Revert "chore: nuke unused SimpleToken contract"
    This reverts commit a1a65b2ab5f7fe7bad099b0b76b2f42a138848cd.

---
+++ contracts/test/SimpleToken.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+
+/**
+ * @title Dummy token for testing purposes
+ */
+contract SimpleToken is ERC20 {
+    uint8 public constant DECIMALS = 18;
+    uint256 public constant INITIAL_SUPPLY = 100000000000000 * (10**uint256(DECIMALS));
+
+    /**
+     * @dev Constructor that gives msg.sender all of existing tokens.
+     */
+    constructor() ERC20("SimpleToken", "SIM") {
+        _mint(msg.sender, INITIAL_SUPPLY);
+    }
+}
